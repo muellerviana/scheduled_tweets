45ca61f1e4b7f56de236ffce7df712d3b92b7362
Müller Viana <muellerviana@gmail.com>
2021-09-14 21:47:15 -0300

    Initial COmmit
    Created routes

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  #Get to /about
+  get "about", to: "about#index"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
